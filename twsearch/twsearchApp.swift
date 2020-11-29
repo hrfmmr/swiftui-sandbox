c261119c5ed4d3b8f8b2ee39bdfa4e1cc1a5e68d
hrfmmr <ladiciushm@gmail.com>
2020-11-29 22:15:15 +0900

    :tada: Add app prj

---
+++ twsearch/twsearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  twsearchApp.swift
+//  twsearch
+//
+//  Created by hrfm mr on 2020/11/29.
+//
+
+import SwiftUI
+
+@main
+struct twsearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
